731491b1c5869f09eefa76395ed4650b98f05eb8
SuryaSankar18 <142087060+SuryaSankar18@users.noreply.github.com>
2025-02-10 10:41:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown(
     <style>
     /* Change the background color */
     .stApp {
-        background-color: #0c0425;  /* Light gray background */
+        background-color: #0009CB;  /* Light gray background */
     }
 
     /* Style the title */
